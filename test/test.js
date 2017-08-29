ecabeb66b24137d857f4ad7de58849af9e6b9c3f
Lawal Lanre E <larrystonegroup@gmail.com>
2017-08-29 15:02:47 +0100

    Write more test cases

--- test/test.js
+++ test/test.js
@@ -10,4 +10,24 @@ describe("Terrestrial Animal class for dog", () => {
     it(`should return "I, ${dog.name}, am eating, and that's why I'm an Animal!" for dog.doEat()"`, () => {
         expect(dog.doEat()).to.equal(`I, ${dog.name}, am eating, and that's why I'm an Animal!`);
     });
+
+    dog.hasTail = true;
+    it(`should return "true" for dog.hasTail"`, () => {
+        expect(dog.hasTail).to.equal(true);
+    });
+ });
+
+const frog = new AquaticAnimal('African Frog');
+describe("Aquatic Animal class for African Frog", () => {
+    it(`should return "I'm swimming in the ${frog.habitat}" for frog.doSwim(${frog.habitat})`, () => {
+        expect(frog.doSwim(frog.habitat)).to.equal(`I'm swimming in the ${frog.habitat}`);
+    });
+
+    it(`should return "No fins" for frog.fins`, () => {
+        expect(frog.fins).to.equal('No fins');
+    });
+
+    it(`should return "streamlined" for dog.bodyType`, () => {
+        expect(frog.bodyType).to.equal('streamlined');
+    });
  });
